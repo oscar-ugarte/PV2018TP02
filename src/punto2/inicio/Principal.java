97febee3ca344cabee6c2f25b133376ce69cee28
oscar-script <scriptoscar@gmail.com>
2018-04-01 22:58:49 -0300

    Agregando la clase Scanner al Principal del punto 2. Usuario Oscar-Script (Oscar Ugarte).

--- src/punto2/inicio/Principal.java
+++ src/punto2/inicio/Principal.java
@@ -1,16 +1,20 @@
 package punto2.inicio;
 
+import java.util.Scanner;
 import punto2.dominio.Cadena;
 
 /**
- *
  * @author Oscar
  */
 public class Principal {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
         Cadena objCadena = new Cadena() ;
-        String frase = "programación visual" ;
+        //String frase = "programación visual" ;
+        String frase ;
         
+        System.out.print("Ingrese una cadena: ");
+        frase = teclado.nextLine();
         //System.out.println("Frase invertida: " + objCadena.invertirFrase(frase) );
         System.out.println("Frase invertida: " + objCadena.invertirFrase2(frase) );
     }
