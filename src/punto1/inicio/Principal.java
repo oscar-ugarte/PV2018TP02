5642eac2c7bcab9bc7a7607a1455dd95ab0cf020
Lenovo <Lenovo@LAPTOP-PCNV5S3S>
2018-04-04 20:52:41 -0300

    Martinez Beltran: Resuelto PT1 con sus clases y paquetes.

--- src/punto1/inicio/Principal.java
+++ src/punto1/inicio/Principal.java
@@ -1,9 +1,23 @@
 package punto1.inicio;
 
+import java.util.Scanner;
+import punto1.dominio.ModificadorDeCadena;
+
 /**
  *
  * @author Oscar
  */
 public class Principal {
     
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        //String cad = "Hola Mundo.";
+        ModificadorDeCadena modificador = new ModificadorDeCadena() ;
+        
+        //System.out.println("Hola Mundo tiene " + modificador.contarVocales(cad) + " vocales." );
+        System.out.print("Ingrese la cadena: ");
+        String cad = teclado.nextLine(); //Por algun motivo los asentos ingresados por teclado no se muestran
+        System.out.println("La cadena es : " + cad );
+        System.out.println("Cantidad de vocales: " + modificador.contarVocales(cad) );
+    }
 }
